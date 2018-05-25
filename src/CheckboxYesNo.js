eb75a4f0a9515c47f45677db61560ac291914e0c
Joren Van Hee <jorenvanhee@gmail.com>
2018-05-25 13:46:22 +0200

    Update target fields on change

--- src/CheckboxYesNo.js
+++ src/CheckboxYesNo.js
@@ -3,11 +3,18 @@ var CheckboxYesNo = function (checkbox, target) {
   this.target = document.querySelector(target);
 
   this.updateTarget();
+  this.bindEventListeners();
 };
 
 CheckboxYesNo.prototype = {
+  bindEventListeners: function () {
+    this.checkbox.addEventListener('change', this.onCheckboxChange.bind(this));
+  },
   updateTarget: function () {
     this.target.value = this.checkbox.checked ? 'yes' : 'no';
+  },
+  onCheckboxChange: function () {
+    this.updateTarget();
   }
 };
 
